b95d5ff6c36bf79f2624979e66c778ec3efd774c
Ron Dear <ron@growthquo.com>
2019-02-12 20:42:42 +0000

    First commit

---
+++ gq-web/src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import './App.css';
+
+var state = {
+  user: {
+    email: "",
+    firstName: "",
+    lastName: "",
+    authed: false
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        
+      </div>
+    );
+  }
+}
+
+export default App;
