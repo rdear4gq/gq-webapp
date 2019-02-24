660b4859c5108e978732a6802a01bdde4cd7b4f2
Ron Dear <ron@growthquo.com>
2019-02-24 03:22:20 +0000

    Implemented Login page and added proxy to work with ExpressJS backend.

--- gq-web/src/App.js
+++ gq-web/src/App.js
@@ -1,20 +1,30 @@
 import React, { Component } from 'react';
-import './App.css';
-
-var state = {
-  user: {
-    email: "",
-    firstName: "",
-    lastName: "",
-    authed: false
-  }
-}
+import './style/App.css';
+import LoginPropmpt from './Components/loginPrompt.js';
+import AppContents from './Components/appContents.js';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      user: {
+        email: "",
+        firstName: "",
+        lastName: "",
+        authed: false,
+        authToken: null
+      }
+    }
+  }
+
   render() {
+
     return (
+      
       <div className="App">
-        
+        {(this.state.user.authed && this.state.user.authToken === null) ? <AppContents /> : <LoginPropmpt /> }
       </div>
     );
   }
---
+++ gq-web/src/Components/appContents.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class AppContents extends Component {
+    
+    render() { 
+        return (
+            <div>Test</div>
+        );
+    }
+}
+ 
+export default AppContents;
