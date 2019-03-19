7bd19a9fb08f1143631a8fa13937cd318c7fbf2c
Ron Dear <ron@growthquo.com>
2019-03-19 18:54:04 +0000

    MOre updates

--- gq-web/src/setupProxy.js
+++
@@ -1,6 +0,0 @@
-const proxy = require('http-proxy-middleware');
-
-module.exports = function(app) {
-    console.log("Setting up the proxy.");
-  app.use(proxy('/api/', { target: 'http://localhost:4005/', changeOrigin: true }));
-};
